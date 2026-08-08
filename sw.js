b0633819139e8612d81219bef0bab4e5cebf7583
yanmushegera-web <yanmushegera@gmail.com>
2026-08-08 15:36:15 +0200

    v7.13.0 - Cloud-Backup und Geraete-Sync

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
 /* Yan App – Service Worker */
-const VERSION = 'v7.12.0';
+const VERSION = 'v7.13.0';
 const CACHE = 'yan-app-' + VERSION;
 const ASSETS = ['./', './index.html', './manifest.webmanifest', './icon-180.png', './icon-192.png', './icon-512.png', './icon-maskable-512.png', './sg-400.woff2', './sg-500.woff2', './sg-700.woff2', './msr.woff2'];
 
